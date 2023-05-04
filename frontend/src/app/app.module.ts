89450f359040d9949e5ce5a4d8a1407ccb9f2bb2
NailtonSSantos <devnailton@gmail.com>
2023-05-04 02:02:44 -0400

    Pastas do Frontend implementadas e arrumadas

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrincipalComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
